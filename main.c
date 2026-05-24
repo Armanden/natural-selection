bbf0522a1d622054d2664ae8323a488d1289bfeb
jesterknightofficial <jesterknight@tutamail.com>
2026-05-24 16:52:15 +0300

    init

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <time.h>
+#include <random.h>
+#include <math.h>
+
+
+gene_types = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z];
+
+typedef struct{
+
+	
+	
+} gene
+
+gene = [a,b,c] //the genes are the numbers in the list
+
+//the list will first be randomly generated and then put in a bubble sort algorithm 
+//and the ones that are less than 3/4 will get eliminated and new genes will be generated using the old genes by \lambda a.\lambda d. e
+// genes will be pitted against each other, a>z, b>y, c>x and such and the better genes will pass on however new genes will be added to the pool via a random selection.
